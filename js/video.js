2e75f1da9f73b2503d307c6120d25a891efa9662
TreyanaR <98418929+TreyanaR@users.noreply.github.com>
2024-11-24 13:21:56 -0500

    top buttons and mute

--- js/video.js
+++ js/video.js
@@ -5,8 +5,52 @@ window.addEventListener("load", function() {
 	video.autoplay = false;
 	video.loop = false;
 });
-
- document.querySelector("#play").addEventListener("click", function() {
+//play video
+ document.querySelector("#play").addEventListener("click", function(){
 	video.play();
+	console.log(video.playbackRate)
  });
+//pause video
+ document.querySelector("#pause").addEventListener("click", function(){
+	video.pause()
+ });
+//reduce speed 
+function reduceSpeed() { 
+	video.playbackRate *= 0.9;
+	console.log(video.playbackRate)
+} 
+document.querySelector("#slower").addEventListener("click", reduceSpeed);
+//speed up
+function speedUp(){
+	video.playbackRate /= .9;
+	console.log(video.playbackRate)
+}
+document.querySelector("#faster").addEventListener("click", speedUp);
+//skip ahead
+function skipAhead(){
+	video.currentTime+=10;
+	console.log(video.currentTime);
+	if (video.currentTime >=video.duration){
+		video.currentTime=0
+		video.pause()
+	}
+}
+document.querySelector("#skip").addEventListener("click",skipAhead)
 
+//mute
+
+let muteButton = document.querySelector("#mute");
+function Mute(){
+	if(video.volume>0){
+		video.volume=0
+		muteButton.innerHTML = "Unmute"
+	}
+	else{
+		video.volume = 1
+		muteButton.innerHTML = "Mute"
+	};
+}
+muteButton.addEventListener("click", Mute);
+
+//slider
+ 
